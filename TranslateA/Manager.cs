82f49690fbb8c45f952bbda445ae67113d03f73b
RelaperCrystal <EndermanLOTR@outlook.com>
2018-05-28 12:34:52 +0800

    s

---
+++ TranslateA/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace TranslateA
+{
+    /// <summary>
+    /// 管理器类。
+    /// </summary>
+    public class Manager
+    {
+        // 有没有人能告诉我是不是可以直接返回Directory.Exists
+        public static bool MCFolderC()
+        {
+            if(Directory.Exists(@".minecraft") == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
